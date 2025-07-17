61531d62b00a1563de040c2b41e9a8bb26272902
koki <2788293436@qq.com>
2025-07-17 18:45:28 +0800

    feat: add stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,10 @@
 /** @type {import('stylelint').Config} */
 
-
 export default {
-  extends: ['stylelint-config-standard-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   ignoreFiles: ['**/node_modules/**', '**/dist/**', 'public'],
   rules: {
     'at-rule-no-unknown': null,
     'color-no-invalid-hex': true,
   },
-}
+};
