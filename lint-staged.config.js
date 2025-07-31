e93d771a1c9fd3e50c93b104cf9f98fcfb886cf5
koki <2788293436@qq.com>
2025-07-31 19:11:27 +0800

    fix: type check

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+const stagedConfig = {
+  '*.{js,jsx}': ['npm run oxlint', 'prettier --cache --write'],
+  '*.{ts,tsx}': [
+    "sh -c 'tsc --noEmit -p tsconfig.app.json'",
+    'npm run oxlint',
+    'prettier --cache --parser=typescript --write',
+  ],
+  '*.{css,less}': ['npm run stylelint', 'prettier --cache --write'],
+  '*.{md,json}': ['prettier --cache --write'],
+};
+
+export default stagedConfig;
